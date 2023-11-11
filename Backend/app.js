ee9801ffe8b86d6f60b7950a9d023cfcd6ab60c7
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2023-11-11 21:17:11 +0530

    tried credentials:true

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://producthunt-frontend.vercel.app");
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, PATCH"
