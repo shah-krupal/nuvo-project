026ea0238fc81d3f96121b5fa610b1c07586b2b9
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2023-11-12 12:19:23 +0530

    tried setHeader

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -231,7 +231,7 @@ router.get("/google/callback",
     failureRedirect: "/login/failed",
   }),
   (req,res)=>{
-	res.setCookie('access_token',"hello",{sameSite:'none',secure:true,httpOnly:true})
+	res.setHeader('access_token',"Hi ji")
 	res.status(200)
 	res.cookie('access_token',"hello",{sameSite:'none',secure:true,httpOnly:true})
 	console.log('here')
